2ee4348ac124e68d032b4e745b440aa9127c7700
jirvine4284 <leadnose06@gmail.com>
2023-12-11 11:42:22 -0800

    working

--- Assets/Scipts/turretCode.cs
+++ Assets/Scipts/turretCode.cs
@@ -8,6 +8,7 @@ public class turretCode : MonoBehaviour
     public bool shooting = false;
     public GameObject turret;
     Collider myCollider;
+    public GameObject target;
 
     // Start is called before the first frame update
     void Start()
@@ -24,11 +25,21 @@ public class turretCode : MonoBehaviour
     {
         turret.GetComponent<Renderer>().enabled = true;
         myCollider.enabled = true;
+        on = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (on)
+        {
+            /*Vector3 targetPosition = turret.transform.position - target.transform.position;
+            float targetRotation = Mathf.Atan(targetPosition.y / targetPosition.x);
+            turret.transform.eulerAngles = new Vector3(0f,0f,targetRotation);
+            turret.transform.rotation = Quaternion.Slerp(Quaternion.Euler(target.transform.position.x, -90f, 0f), Quaternion.LookRotation(target.transform.position), 2f * Time.deltaTime);*/
+
+
+
+        }
     }
 }
